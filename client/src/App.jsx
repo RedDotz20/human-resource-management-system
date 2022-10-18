ae06ea3ea42ec6f0c919cc83f93de60d5e2d8f4c
Carlos Tabangay <carlos_tabangay@outlook.com>
2022-10-19 00:52:49 +0800

    InsertData component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import {
 import Add from "@mui/icons-material/Add";
 import Modal from "./components/Modal/Modal";
 import SearchBar from "./components/SearchBar/SearchBar";
+import InsertData from "./components/InsertForm/InsertData";
 
 export default function App() {
 	//* Refresh Table onChange event
@@ -108,7 +109,9 @@ export default function App() {
 
 	return (
 		<div className="App">
-			{modalOpen && <Modal setOpenModal={setModalOpen} />}
+			{modalOpen && (
+				<InsertData setRefresh={setRefresh} setOpenModal={setModalOpen} />
+			)}
 
 			<h1 className="header">Human Resource Management System</h1>
 			<div className="App-container">
@@ -128,57 +131,6 @@ export default function App() {
 							</Button>
 						</Stack>
 					</div>
-
-					<TextField
-						label="First Name"
-						variant="standard"
-						size="small"
-						name="firstName"
-						onChange={handleChange}
-					/>
-
-					<TextField
-						label="Last Name"
-						name="lastName"
-						variant="standard"
-						size="small"
-						onChange={handleChange}
-					/>
-
-					<TextField
-						label="Age"
-						variant="standard"
-						name="age"
-						type="number"
-						size="small"
-						onChange={handleChange}
-					/>
-
-					<FormControl>
-						<FormLabel>Sex </FormLabel>
-						<RadioGroup row name="sex" onChange={handleChange}>
-							<FormControlLabel label="Male" value="M" control={<Radio />} />
-							<FormControlLabel label="Female" value="F" control={<Radio />} />
-						</RadioGroup>
-					</FormControl>
-
-					<TextField
-						label="Phone Number"
-						variant="standard"
-						name="phoneNumber"
-						size="small"
-						onChange={handleChange}
-					/>
-
-					<button
-						className="insert-form"
-						onClick={() => {
-							submitForm();
-							setRefresh(!refresh);
-						}}
-					>
-						Insert Data
-					</button>
 
 					<table className="content-table">
 						<thead>
---
+++ client/src/components/InsertForm/InsertData.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import CloseIcon from "@mui/icons-material/Close";
+import "./InsertData.css";
+import {
+	Button,
+	TextField,
+	Radio,
+	RadioGroup,
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+} from "@mui/material";
+
+const PORT = 3000;
+const URL = `http://localhost:${PORT}`;
+
+export default function InsertData({ setRefresh, setOpenModal }) {
+	function SubmitData() {
+		Axios.post(`${URL}/insert`, {
+			firstName: firstName,
+			lastName: lastName,
+			age: age,
+			sex: sex,
+			phoneNumber: phoneNumber,
+		});
+	}
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [age, setAge] = useState(0);
+	const [sex, setSex] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState(0);
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		name == "firstName" && setFirstName(() => value);
+		name == "lastName" && setLastName(() => value);
+		name == "age" && setAge(() => value);
+		name == "sex" && setSex(() => value);
+		name == "phoneNumber" && setPhoneNumber(() => value);
+	}
+
+	return (
+		<div className="modalBackground">
+			<div className="modalContainer">
+				<CloseIcon
+					className="titleCloseBtn"
+					onClick={() => {
+						setOpenModal(false);
+						console.log("closed");
+					}}
+				/>
+
+				<h1>Insert Employee</h1>
+				<TextField
+					label="First Name"
+					variant="standard"
+					size="small"
+					name="firstName"
+					onChange={handleChange}
+				/>
+
+				<TextField
+					label="Last Name"
+					name="lastName"
+					variant="standard"
+					size="small"
+					onChange={handleChange}
+				/>
+
+				<TextField
+					label="Age"
+					variant="standard"
+					name="age"
+					type="number"
+					size="small"
+					onChange={handleChange}
+				/>
+
+				<FormControl>
+					<FormLabel>Sex </FormLabel>
+					<RadioGroup row name="sex" onChange={handleChange}>
+						<FormControlLabel label="Male" value="M" control={<Radio />} />
+						<FormControlLabel label="Female" value="F" control={<Radio />} />
+					</RadioGroup>
+				</FormControl>
+
+				<TextField
+					label="Phone Number"
+					variant="standard"
+					name="phoneNumber"
+					size="small"
+					onChange={handleChange}
+				/>
+
+				{/* <button
+					className="insert-form"
+					onClick={() => {
+						SubmitData();
+						setRefresh(!refresh);
+					}}
+				>
+					Insert Data
+				</button> */}
+
+				<Button
+					className="insert-form"
+					variant="contained"
+					size="small"
+					onClick={() => {
+						SubmitData();
+						setRefresh(!setRefresh);
+					}}
+				>
+					Confirm
+				</Button>
+			</div>
+		</div>
+	);
+}
