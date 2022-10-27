797ec3755d975a319e800e79681bf04016ff1e2a
Carlos Tabangay <carlos_tabangay@outlook.com>
2022-10-27 21:17:55 +0800

    server.js configured

--- server/app.js
+++ server/app.js
@@ -1,13 +1,8 @@
 const { notFound, errorHandler } = require("./middleware/index"),
-	con = require("./database/connection"),
 	bodyParser = require("body-parser"),
 	express = require("express"),
-	mysql = require("mysql2"),
 	cors = require("cors"),
 	app = express();
-
-require("dotenv").config();
-const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +16,4 @@ app.use(require("./routes/delete")); //? Delete Employee
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-	console.log(`Runniing on http://localhost:${PORT}`);
-});
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
-app.listen(PORT, () => {
+const app = require("./app");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (error) => {
+	if (error) throw error;
 	console.log(`runniing on http://localhost:${PORT}`);
 });
