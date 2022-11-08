219e8c4bd32ad19da3a9f3d36983add9bb373a6f
Carlos Tabangay <carlos_tabangay@outlook.com>
2022-11-08 21:19:30 +0800

    confirm component initialized

---
+++ client/src/components/Modal/ConfirmModal/Confirm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+
+export default function Confirm() {
+	return (
+		<>
+			<CloseIcon className="closeBtn" />
+			Confirm
+		</>
+	);
+}
