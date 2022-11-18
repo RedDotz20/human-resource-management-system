7600257ed65d6d8ff3428902226b23c2256c11d0
Carlos Tabangay <carlos_tabangay@outlook.com>
2022-11-18 16:20:10 +0800

    employeelist css removed

--- client/src/components/EmployeeList/EmployeeList.jsx
+++ client/src/components/EmployeeList/EmployeeList.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
-import "./EmployeeList.css";
 
 export default function EmployeeDataList({
 	employees,
