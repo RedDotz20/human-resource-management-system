12e9767243d2d494507ad2a8779d7bd33c9d1cdc
Carlos Tabangay <carlos_tabangay@outlook.com>
2022-11-21 12:51:11 +0800

    table overflow fixed

--- client/src/components/EmployeeList/EmployeeList.jsx
+++ client/src/components/EmployeeList/EmployeeList.jsx
@@ -5,7 +5,7 @@ import UpdateBtn from "../Button/UpdateBtn";
 const TableHeader = () => {
 	return (
 		<thead>
-			<tr className="child-td:py-1 child-td:px-4 bg-[#089879] text-white font-bold text-center">
+			<tr className="sticky top-0 z-[1] child-td:py-1 child-td:px-4 bg-[#089879] text-white font-bold text-center">
 				<td>ID</td>
 				<td>FIRST NAME</td>
 				<td>LAST NAME</td>
@@ -36,44 +36,46 @@ export default function EmployeeDataList({
 	setUpdateModal,
 }) {
 	return (
-		<table className="border-collapse my-8 text-base w-full shadow-xl child-th:py-1 child-th:px-4 max-h-min">
-			<TableHeader />
+		<div className="overflow-y-scroll scroll-width h-[18.5rem] border-collapse my-8 text-base w-full shadow-xl child-th:py-1 child-th:px-4 max-h-min">
+			<table className="w-full">
+				<TableHeader />
 
-			<tbody className="b-bottom b-color-even b-color-last">
-				{employees.length <= 0 ? (
-					<EmptyTable />
-				) : (
-					employees.map((values) => {
-						const { id, firstName, lastName, age, sex, phoneNumber } = values;
-						return (
-							<tr
-								className="child-td:py-1 child-td:px-4 child-td:text-center"
-								key={id}
-							>
-								<td>{id}</td>
-								<td>{firstName}</td>
-								<td>{lastName}</td>
-								<td>{age}</td>
-								<td>{sex}</td>
-								<td>{phoneNumber}</td>
-								<td>
-									<UpdateBtn
-										setUpdateModal={setUpdateModal}
-										setUpdateId={setUpdateId}
-										updateId={id}
-									/>
+				<tbody className="b-bottom b-color-even b-color-last">
+					{employees.length <= 0 ? (
+						<EmptyTable />
+					) : (
+						employees.map((values) => {
+							const { id, firstName, lastName, age, sex, phoneNumber } = values;
+							return (
+								<tr
+									className="child-td:py-1 child-td:px-4 child-td:text-center"
+									key={id}
+								>
+									<td>{id}</td>
+									<td>{firstName}</td>
+									<td>{lastName}</td>
+									<td>{age}</td>
+									<td>{sex}</td>
+									<td>{phoneNumber}</td>
+									<td>
+										<UpdateBtn
+											setUpdateModal={setUpdateModal}
+											setUpdateId={setUpdateId}
+											updateId={id}
+										/>
 
-									<DeleteBtn
-										setDeleteModal={setDeleteModal}
-										setDeleteId={setDeleteId}
-										deleteId={id}
-									/>
-								</td>
-							</tr>
-						);
-					})
-				)}
-			</tbody>
-		</table>
+										<DeleteBtn
+											setDeleteModal={setDeleteModal}
+											setDeleteId={setDeleteId}
+											deleteId={id}
+										/>
+									</td>
+								</tr>
+							);
+						})
+					)}
+				</tbody>
+			</table>
+		</div>
 	);
 }
