29379b209a51b2a00993b9227924a5d6273c8a8e
RedDotz20 <carlos_tabangay@outlook.com>
2023-01-02 10:54:28 +0800

    delete: client delete

--- client/src/contexts/Contexts.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const GetValueContext = createContext(null);
-export const ModalContext = createContext(null);
--- client/src/utilities/formatString.js
+++
@@ -1,5 +0,0 @@
-export function formatPascalCase(string) {
-	return string
-		.toLowerCase()
-		.replace(/(^\w{1})|(\s+\w{1})/g, (character) => character.toUpperCase());
-}
