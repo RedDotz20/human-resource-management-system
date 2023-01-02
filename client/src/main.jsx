c96f06afb6b71eea1f52837b72bc558c88e1235b
RedDotz20 <carlos_tabangay@outlook.com>
2023-01-02 10:29:02 +0800

    refactor: unecessary import remove

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,40 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-
-export const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false,
-			refetchOnMount: false,
-			staleTime: Infinity,
-		},
-	},
-});
 
 let container = null;
-
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
 	if (!container) {
 		container = document.getElementById("root");
 		ReactDOM.createRoot(container).render(
 			<React.StrictMode>
-				<QueryClientProvider client={queryClient}>
-					<App />
-					<ReactQueryDevtools initialIsOpen={true} />
-				</QueryClientProvider>
+				<App />
 			</React.StrictMode>
 		);
 	}
 });
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-// 	<React.StrictMode>
-// 		<QueryClientProvider client={queryClient}>
-// 			<App />
-// 			<ReactQueryDevtools initialIsOpen={true} />
-// 		</QueryClientProvider>
-// 	</React.StrictMode>
-// );
