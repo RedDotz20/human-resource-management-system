6ed807fdbe9c7ef3b8a6f837e3425c8593205b5f
RedDotz20 <carlos_tabangay@outlook.com>
2023-06-04 21:47:22 +0800

    remove: unused dependency

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/index.css';
 
 let container: any = null;
-document.addEventListener("DOMContentLoaded", () => {
-	container = container || document.getElementById("root");
+
+document.addEventListener('DOMContentLoaded', () => {
+	container = container || document.getElementById('root');
 	ReactDOM.createRoot(container as HTMLElement).render(
 		<React.StrictMode>
 			<App />
